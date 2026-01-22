ee9776bb210a7ed88137d052dce12565bf94bee5
unknown <rishebs123456@gmail.com>
2026-01-22 15:45:13 +0530

    Add django-cors-headers to requirements

--- lpcrm/settings.py
+++ lpcrm/settings.py
@@ -59,6 +59,7 @@ CRONJOBS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
