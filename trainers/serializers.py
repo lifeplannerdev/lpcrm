f7b0967668a5ac89836b7ebc0939b4c3913cb1b6
lifeplannerdev <lifeplannermedia@gmail.com>
2026-02-05 12:16:37 +0530

    Update serializers.py

--- trainers/serializers.py
+++ trainers/serializers.py
@@ -1,20 +1,22 @@
 from rest_framework import serializers
 from .models import Trainer, Student, Attendance
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Trainer Serializer
 class TrainerSerializer(serializers.ModelSerializer):
     user_name = serializers.CharField(source='user.get_full_name', read_only=True)
     email = serializers.CharField(source='user.email', read_only=True)
-
+    
     class Meta:
         model = Trainer
         fields = ['id', 'user', 'user_name', 'email', 'drive_link', 'status']
 
-
 # Student Serializer
 class StudentSerializer(serializers.ModelSerializer):
     trainer_name = serializers.CharField(source='trainer.user.get_full_name', read_only=True)
-
+    
     class Meta:
         model = Student
         fields = [
@@ -23,12 +25,11 @@ class StudentSerializer(serializers.ModelSerializer):
             'email', 'phone_number', 'drive_link', 'student_class'
         ]
 
-
 # Attendance Serializer
 class AttendanceSerializer(serializers.ModelSerializer):
     student_name = serializers.CharField(source='student.name', read_only=True)
     trainer_name = serializers.CharField(source='trainer.user.get_full_name', read_only=True)
-
+    
     class Meta:
         model = Attendance
         fields = [
@@ -36,3 +37,22 @@ class AttendanceSerializer(serializers.ModelSerializer):
             'student', 'student_name',
             'status', 'marked_at'
         ]
+
+# NEW: Trainer User Serializer (for listing all users with TRAINER role)
+class TrainerUserSerializer(serializers.ModelSerializer):
+    """Serializer for listing users with TRAINER role"""
+    full_name = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = User
+        fields = [
+            'id', 'username', 'email', 'first_name', 'last_name',
+            'full_name', 'role', 'phone', 'is_active'
+        ]
+    
+    def get_full_name(self, obj):
+        if obj.first_name and obj.last_name:
+            return f"{obj.first_name} {obj.last_name}"
+        elif obj.first_name:
+            return obj.first_name
+        return obj.username
