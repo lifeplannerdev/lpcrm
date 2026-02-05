16120a003d8d366bf203129fdec52ce984763421
lifeplannerdev <lifeplannermedia@gmail.com>
2026-02-05 11:24:42 +0530

    Add IsHRorAccounts permission class

--- hr/permissions.py
+++ hr/permissions.py
@@ -1,5 +1,9 @@
 from rest_framework.permissions import BasePermission
 
-class IsHR(BasePermission):
+class IsHRorAccounts(BasePermission):
     def has_permission(self, request, view):
-        return request.user and request.user.is_authenticated and request.user.is_hr
+        return (
+            request.user
+            and request.user.is_authenticated
+            and request.user.role in ["HR", "ACCOUNTS"]
+        )
