ab59072fb241962f898ee4f1134743721f1d69b9
lifeplannerdev <lifeplannermedia@gmail.com>
2026-02-09 15:21:33 +0530

    Add view URL field and method to serializer

--- reports/serializers.py
+++ reports/serializers.py
@@ -5,8 +5,9 @@ from .models import DailyReport
 class DailyReportSerializer(serializers.ModelSerializer):
     user_name = serializers.CharField(source="user.get_full_name", read_only=True)
     file_url = serializers.SerializerMethodField()
+    view_url = serializers.SerializerMethodField()  # NEW: Add view URL
     reviewed_by_name = serializers.CharField(source="reviewed_by.get_full_name", read_only=True)
-
+    
     class Meta:
         model = DailyReport
         fields = [
@@ -18,6 +19,7 @@ class DailyReportSerializer(serializers.ModelSerializer):
             "report_text",
             "attached_file",
             "file_url",
+            "view_url",  # NEW: Add to fields
             "report_date",
             "status",
             "review_comment",
@@ -34,9 +36,20 @@ class DailyReportSerializer(serializers.ModelSerializer):
             "created_at",
             "updated_at",
         ]
-
+    
     def get_file_url(self, obj):
+        """URL for downloading the file"""
         request = self.context.get("request")
         if obj.attached_file and request:
             return request.build_absolute_uri(obj.attached_file.url)
+        return None
+    
+    def get_view_url(self, obj):
+        """URL for viewing the file inline (NEW METHOD)"""
+        request = self.context.get("request")
+        if obj.attached_file and request:
+            # Build URL to the view-file endpoint
+            from django.urls import reverse
+            view_path = reverse('view-report-file', kwargs={'pk': obj.id})
+            return request.build_absolute_uri(view_path)
         return None
