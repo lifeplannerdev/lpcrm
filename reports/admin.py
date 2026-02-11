6212b23090dcab591ae3b1c4ff719ebb4db914fc
unknown <rishebs123456@gmail.com>
2026-02-11 17:02:12 +0530

    updated reports page can attch multple files

--- reports/admin.py
+++ reports/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import DailyReport
+from .models import DailyReport,DailyReportAttachment
 # Register your models here.
 
 admin.site.register(DailyReport)
+admin.site.register(DailyReportAttachment)
