6804da568ae0bf7e525b15616c9a4898356eade5
unknown <rishebs123456@gmail.com>
2026-03-09 10:45:25 +0530

    update

--- telephony/urls.py
+++ telephony/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import voxbay_webhook
+from .views import voxbay_webhook, call_logs_list
 
 urlpatterns = [
-    path("voxbay/webhook/", voxbay_webhook),
+    path("voxbay/webhook/",    voxbay_webhook), 
+    path("voxbay/call-logs/",  call_logs_list),  
 ]
